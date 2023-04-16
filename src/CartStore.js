0913fd9416784b867933ea975179f5fb5e9ef6da
María Mira <maria.alaves93@gmail.com>
2023-04-16 20:37:14 +0200

    Corregidas funciones de incrementar/decrementar producto en CartStore.js. Añadida función de Eliminar Producto.

--- src/CartStore.js
+++ src/CartStore.js
@@ -29,11 +29,23 @@ export const useCartProducts = defineStore('cart', {
         this.items.push(item);
       } //esto no iría aquí, iría en ProductList y en Recomendados
     },
-    increaseProduct(item) { },
-    decreaseProduct(item) { },
-
+    increaseProduct(item) {
+      let index = this.items.findIndex(product => product.id == item.id);
+      if (index !== -1) {
+        this.items[index].quantity += 1;
+      }
+    },
+    decreaseProduct(item) {
+      let index = this.items.findIndex(product => product.id == item.id);
+      if (index !== -1) {
+        this.items[index].quantity -= 1;
+        if (this.items[index].quantity === 0) {
+          this.items = this.items.filter(product => product.id !== item.id);
+        }
+      }
+    },
     removeProduct(item) {
-
+      this.items = this.items.filter(product => product.id !== item.id);
     },
 
     payProducts() {
