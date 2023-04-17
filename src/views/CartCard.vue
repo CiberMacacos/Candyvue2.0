4b06ee8346b112643cccf1e560bb81df5363d674
María Mira <maria.alaves93@gmail.com>
2023-04-17 15:57:41 +0200

    Desarrollado componente CartCard para integrar en Cartkoa y hacer v-for para imprimir datos en carrito.

---
+++ src/views/CartCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="w-full flex justify-around items-center text-center md:gap-4 lg:gap-46 sm:p-4 md:p-6">
+    <div class="flex flex-col gap-3 hidden sm:block">
+      <h1 class="font-bold sm:text-sm ">Productos</h1>
+      <img class="w-12 md:w-28" src="{{ this.cartProducts[i].image }}" alt="{{ this.cartProducts[i].name }}">
+    </div>
+    <div class="flex flex-col md:gap-3 items-center text-center w-12">
+      <h1 class="font-bold text-sm md:text-base">Nombre</h1>
+      <h2 class="sm:text-sm sm:gap4">{{ this.cartProducts[i].name }}</h2>
+    </div>
+    <div class="flex flex-col gap-3 items-center hidden sm:block">
+      <h2 class="font-bold text-sm md:text-base">Precio</h2>
+      <p class="sm:text-sm">{{ this.cartProducts[i].price }}</p>
+    </div>
+    <div class="flex flex-col gap-3 items-center">
+      <h1 class="font-bold text-sm md:text-base">Cantidad</h1>
+      <div class="flex flex-row gap-6">
+        <button
+          class="bg-pink-300 w-8 h-8 hover:bg-blue-300 text-white font-bold py-2 rounded-full text-sm items-center hidden sm:block">-</button>
+        <p class="sm:text-sm">{{ this.cartProducts[i].quantity }}</p>
+        <button
+          class="bg-pink-300 w-8 h-8 hover:bg-blue-300 text-white font-bold py-2 rounded-full text-sm items-center hidden sm:block">+</button>
+      </div>
+    </div>
+    <div class="flex flex-col gap-3">
+      <h2 class="font-bold text-sm md:text-base">Total</h2>
+      <h2 class="sm:text-sm">{{ this.cartProducts[i].price *
+              this.cartProducts[i].quantity }}€</h2>
+    </div>
+    <div class="flex flex-col items-center">
+      <button @click="this.removeProduct()">
+        <img class="w-4 sm:w-6 md:w-10" src="assets/icon/basura.png" alt="papelera">
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
--- src/views/Cartkoa.vue
+++ src/views/Cartkoa.vue
@@ -2,10 +2,11 @@
 import Breadcrumb from '../components/Breadcrumb.vue'
 import { useCartProducts } from '../CartStore.js'
 import { mapState, mapActions } from 'pinia'
+import CartCard from './CartCard.vue'
 
 export default {
   name: 'Cart',
-  components: { Breadcrumb },
+  components: { Breadcrumb, CartCard },
   computed: {
     ...mapState(useCartProducts, ['cartProducts'])
   },
@@ -20,44 +21,44 @@ export default {
 
 <template>
   <!--Productos-->
-  <div class="bg-pink-50 md:rounded-lg w-full h-auto flex flex-col">
-    <div class="mx-10 mt-10">
-      <div id="productsCart" class="bg-white w-full rounded-lg flex flex-col md:gap-8">
-        <div id="productsCart"
-          class="w-full flex justify-around items-center text-center md:gap-4 lg:gap-46 sm:p-4 md:p-6">
-          <div class="flex flex-col gap-3 hidden sm:block">
-            <h1 class="font-bold sm:text-sm ">Productos</h1>
-            <img class="w-12 md:w-28" src="{{ this.cartProducts[i].image }}" alt="{{ this.cartProducts[i].name }}">
-          </div>
-          <div class="flex flex-col md:gap-3 items-center text-center w-12">
-            <h1 class="font-bold text-sm md:text-base">Nombre</h1>
-            <h2 class="sm:text-sm sm:gap4">{{ this.cartProducts[i].name }}</h2>
-          </div>
-          <div class="flex flex-col gap-3 items-center hidden sm:block">
-            <h2 class="font-bold text-sm md:text-base">Precio</h2>
-            <p class="sm:text-sm">{{ this.cartProducts[i].price }}</p>
-          </div>
-          <div class="flex flex-col gap-3 items-center">
-            <h1 class="font-bold text-sm md:text-base">Cantidad</h1>
-            <div class="flex flex-row gap-6">
-              <button
-                class="bg-pink-300 w-8 h-8 hover:bg-blue-300 text-white font-bold py-2 rounded-full text-sm items-center hidden sm:block">-</button>
-              <p class="sm:text-sm">{{ this.cartProducts[i].quantity }}</p>
-              <button
-                class="bg-pink-300 w-8 h-8 hover:bg-blue-300 text-white font-bold py-2 rounded-full text-sm items-center hidden sm:block">+</button>
-            </div>
-          </div>
-          <div class="flex flex-col gap-3">
-            <h2 class="font-bold text-sm md:text-base">Total</h2>
-            <h2 class="sm:text-sm">{{ this.cartProducts[i].price *
-              this.cartProducts[i].quantity }}€</h2>
-          </div>
-          <div class="flex flex-col items-center">
-            <button @click="this.removeProduct()">
-              <img class="w-4 sm:w-6 md:w-10" src="assets/icon/basura.png" alt="papelera">
-            </button>
-          </div>
+<div class="bg-pink-50 md:rounded-lg w-full h-auto flex flex-col">
+  <div class="mx-10 mt-10">
+    <div class="bg-white w-full rounded-lg flex flex-col md:gap-8">
+      <CartCard v-for="item in this.CartProducts" />
+      <!--<div class="w-full flex justify-around items-center text-center md:gap-4 lg:gap-46 sm:p-4 md:p-6">
+        <div class="flex flex-col gap-3 hidden sm:block">
+          <h1 class="font-bold sm:text-sm ">Productos</h1>
+          <img class="w-12 md:w-28" src="{{ this.cartProducts[i].image }}" alt="{{ this.cartProducts[i].name }}">
         </div>
+        <div class="flex flex-col md:gap-3 items-center text-center w-12">
+          <h1 class="font-bold text-sm md:text-base">Nombre</h1>
+          <h2 class="sm:text-sm sm:gap4">{{ this.cartProducts[i].name }}</h2>
+        </div>
+        <div class="flex flex-col gap-3 items-center hidden sm:block">
+          <h2 class="font-bold text-sm md:text-base">Precio</h2>
+          <p class="sm:text-sm">{{ this.cartProducts[i].price }}</p>
+        </div>
+            <div class="flex flex-col gap-3 items-center">
+              <h1 class="font-bold text-sm md:text-base">Cantidad</h1>
+              <div class="flex flex-row gap-6">
+                <button
+                  class="bg-pink-300 w-8 h-8 hover:bg-blue-300 text-white font-bold py-2 rounded-full text-sm items-center hidden sm:block">-</button>
+                <p class="sm:text-sm">{{ this.cartProducts[i].quantity }}</p>
+                <button
+                  class="bg-pink-300 w-8 h-8 hover:bg-blue-300 text-white font-bold py-2 rounded-full text-sm items-center hidden sm:block">+</button>
+              </div>
+            </div>
+            <div class="flex flex-col gap-3">
+              <h2 class="font-bold text-sm md:text-base">Total</h2>
+              <h2 class="sm:text-sm">{{ this.cartProducts[i].price *
+                this.cartProducts[i].quantity }}€</h2>
+            </div>
+            <div class="flex flex-col items-center">
+              <button @click="this.removeProduct()">
+                <img class="w-4 sm:w-6 md:w-10" src="assets/icon/basura.png" alt="papelera">
+              </button>
+            </div>
+          </div>-->
       </div>
       <!--Subtotal productos-->
       <div class="flex flex-col w-100% h50 gap-4 md:items-end font-bold my-4 md:m-10">
